39bc348704cc518979e8d39b3b6cddca845f47c7
erez <erez@Erez>
2016-03-08 22:04:41 +0200

    עידכון שלישי..במחלקה גרףאלגו יש מתודה שמחזירה את המסלול הקצר ביותר.. צריך להחזיר מחרוזת של המסלול ולעשות את המתודה עם הרשימה השחורה

--- erez_Ex0/src/erez_Ex0/Graph_algo.java
+++ erez_Ex0/src/erez_Ex0/Graph_algo.java
@@ -1,11 +1,16 @@
 package erez_Ex0;
 
 public class Graph_algo {
-	private double [] dist;
-	private	Graph G;
-	private boolean [] visited ;
+	Graph G;
 
 	public Graph_algo(Graph G) {
-
+		this.G = G;
+	}
+	public double MinDistNodes(String name_file,int start,int end){
+		Graph g = new Graph(name_file,start);
+		return g.MinDistanceTwoNode(end);
+	}
+	public double MinDistNodesWithBL(String name_file,int start,int end){
+		
 	}
 }
